acf3a0a12a3cfba2763fc950ccbdffcddd91097c
szludora <szlucska.dora@gmail.com>
2023-11-02 12:48:53 +0100

    üzenet írása modal szépítése

--- scripts/uzenetek.js
+++ scripts/uzenetek.js
@@ -1,19 +1,18 @@
 $(function () {
-    var modal = $("#myModal");
-  
-    $(".btn-compose").on("click", function () {
-      modal.modal('show');
-      console.log("nyit");
-    });
-  
-    modal.find(".close").on("click", function () {
-      modal.modal("hide");
-    });
-  
-    $(document).on("click", function (event) {
-      if (event.target === modal[0]) {
-        modal.modal("hide");
-      }
-    });
+  var modal = $("#myModal");
+
+  $(".btn-compose").on("click", function () {
+    modal.modal("show");
+    console.log("nyit");
   });
-  
+
+  modal.find(".close").on("click", function () {
+    modal.modal("hide");
+  });
+
+  $(document).on("click", function (event) {
+    if (event.target === modal[0]) {
+      modal.modal("hide");
+    }
+  });
+});
