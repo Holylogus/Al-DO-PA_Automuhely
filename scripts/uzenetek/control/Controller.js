ac3d17466d3cf27ed14d1b1e82cffbadb368498c
szludora <szlucska.dora@gmail.com>
2023-11-02 20:15:56 +0100

    MVC beáll.

---
+++ scripts/uzenetek/control/Controller.js
@@ -0,0 +1,10 @@
+import Uzenet from "../model/Uzenet.js";
+import InfoView from "../view/InfoView.js"
+
+class Controller {
+  constructor() {
+    this.InfoView = new this.InfoView();
+  }
+}
+
+$(function () {});
