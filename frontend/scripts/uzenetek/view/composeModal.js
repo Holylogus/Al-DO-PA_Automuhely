e5733e64c1b0b14a34e2f4c3ce1ecd6ca0e6d856
szludora <szlucska.dora@gmail.com>
2024-01-14 12:55:18 +0100

    modal backdrop z-index -1

--- frontend/scripts/uzenetek/view/composeModal.js
+++ frontend/scripts/uzenetek/view/composeModal.js
@@ -1,59 +1,59 @@
 class ComposeModal {
-    constructor() {
-      this.compose = $(".btn-compose");
-      this.modal = $("#myModal");
-      this.fileInput = $(".fileinput-button");
-      this.csatoltFajlok = $(".csatoltFajlok");
-      this.modalHide = this.modal.find(".close");
-      this.figyel();
-    }
-  
-    figyel() {
-      this.compose.on("click", () => {
-        this.mutat();
-      });
-  
-      this.modalHide.on("click", () => {
-        this.elrejt();
-      });
-  
-      $(document).on("click", (event) => {
-        if (event.target === this.modal[0]) {
-          this.elrejt();
-        }
-      });
-  
-      this.fileInput.on("change", () => {
-        this.csatolmanyHozzaad();
-      });
-  
-      this.csatoltFajlok.on("click", ".torolGomb", () => {
-        this.csatolmanyTorol();
-      });
-    }
-  
-    mutat() {
-      this.modal.modal("show");
-    }
-  
-    elrejt() {
-      this.modal.modal("hide");
-    }
-  
-    csatolmanyHozzaad() {
-      var fajlUt = this.fileInput.children("input").val();
-      var fajlNev = fajlUt.replace("C:\\fakepath\\", " ");
-      this.csatoltFajlok.append(
-        `<i class="fa fa-paperclip"></i>`,
-        fajlNev,
-        `<span class="torolGomb">&nbsp;❌&nbsp;</span>`
-      );
-    }
-  
-    csatolmanyTorol() {
-      console.log("Törlés gomb");
-    }
+  constructor() {
+    this.compose = $(".btn-compose");
+    this.modal = $("#myModal");
+    this.fileInput = $(".fileinput-button");
+    this.csatoltFajlok = $(".csatoltFajlok");
+    this.modalHide = this.modal.find(".close");
+
+
+    this.compose.on("click", () => {
+      this.mutat();
+    });
+
+    this.modalHide.on("click", () => {
+      this.elrejt();
+    });
+
+     $(document).on("click", (event) => {
+          console.log(event.target);
+
+       if (event.target === this.modal) {
+         this.elrejt();
+       }
+     });
+
+    this.fileInput.on("change", () => {
+      this.csatolmanyHozzaad();
+    });
+
+    this.csatoltFajlok.on("click", ".torolGomb", () => {
+      this.csatolmanyTorol();
+    });
   }
-  
-  export default ComposeModal;
-  
+
+  mutat() {
+    this.modal.modal("show");
+  }
+
+  elrejt() {
+    console.log("elrejt");
+    this.modal.modal("hide");
+  }
+
+  csatolmanyHozzaad() {
+    var fajlUt = this.fileInput.children("input").val();
+    var fajlNev = fajlUt.replace("C:\\fakepath\\", " ");
+    this.csatoltFajlok.append(
+      `<i class="fa fa-paperclip"></i>`,
+      fajlNev,
+      `<span class="torolGomb">&nbsp;❌&nbsp;</span>`
+    );
+  }
+
+  csatolmanyTorol() {
+    console.log("Törlés gomb");
+  }
+}
+
+export default ComposeModal;
