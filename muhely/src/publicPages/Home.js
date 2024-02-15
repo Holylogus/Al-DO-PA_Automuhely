017aacfc696dae59a1a33732264f458dbfacc4e1
szludora <szlucska.dora@gmail.com>
2024-02-15 22:39:04 +0100

    navbar javítása, -akciok.css -menu.css

--- muhely/src/publicPages/Home.js
+++ muhely/src/publicPages/Home.js
@@ -1,83 +1,83 @@
 import "./styles/style.css"
-import "./styles/header.css"
-import "./styles/szolgaltatasok.css"
+import "./styles/video.css"
+import "./styles/home.css"
 
 export default function Home() {
   return (
     <>
-    <div className="myHeader"><video autoPlay controls loop muted>
+  <video autoPlay controls loop muted>
         <source src="/images/header/headerVideo3.mp4" />
-      </video></div>
-      <div className="blockelem">
+      </video>
+      <div className="blockElem">
 
       
-      <div className="containers">
-        <div className="container">
+      <div className="homeKepek">
+        <div className="homeKep">
           <img src="/images/szolgaltatasok/Akkumulator_csere.jpg" alt="Avatar" className="image" />
           <div className="middle">
             <div className="text">Akkumulátor</div>
           </div>  
         </div>
 
-        <div className="container">
+        <div className="homeKep">
           <img src="/images/szolgaltatasok/futomubeallítas.jpg" alt="Avatar" className="image" />
-          <div className="middle1">
+          <div className="middle">
             <div className="text">Futómű</div>
           </div>  
         </div>
 
-        <div className="container">
+        <div className="homeKep">
           <img src="/images/szolgaltatasok/időszakos szerviz.jpg" alt="Avatar" className="image" />
-          <div className="middle2">
+          <div className="middle">
             <div className="text">Időszakos szerviz</div>
           </div>  
         </div>
 
-        <div className="container">
+        <div className="homeKep">
           <img src="/images/szolgaltatasok/Klíma.jpg" alt="Avatar" className="image" />
-          <div className="middle3">
+          <div className="middle">
             <div className="text">Klíma</div>
           </div>  
         </div>
         
-        <div className="container">
+        <div className="homeKep">
           <img src="/images/szolgaltatasok/atvizsgalas.jpg" alt="Avatar" className="image" />
-          <div className="middle4">
+          <div className="middle">
             <div className="text">Ingyenes műszaki felmérés</div>
           </div>  
         </div>
       
-        <div className="container">
+        <div className="homeKep">
           <img src="/images/szolgaltatasok/fekcsere.jpg" alt="Avatar" className="image" />
-          <div className="middle5">
+          <div className="middle">
             <div className="text">Fékrendszer</div>
           </div>  
         </div>
         
-        <div className="container"> 
+        <div className="homeKep"> 
           <img src="/images/szolgaltatasok/gumicsere.jpg" alt="Avatar" className="image" />
-          <div className="middle6">
+          <div className="middle">
             <div className="text">Gumiabroncs,gumicsere</div>
           </div>  
         </div>
         
-        <div className="container">
+        <div className="homeKep">
           <img src="/images/szolgaltatasok/kipufogó.jpg" alt="Avatar" className="image" />
-          <div className="middle7">
+          <div className="middle">
             <div className="text">Kipufogó</div>
           </div>  
         </div>
 
-        <div className="container">
+        <div className="homeKep">
           <img src="/images/szolgaltatasok/muszaki vizsga.jpg" alt="Avatar" className="image" />
-          <div className="middle8">
+          <div className="middle">
             <div className="text">Műszaki vizsga</div>
           </div>  
         </div>
         
-        <div className="container"> 
+        <div className="homeKep"> 
           <img src="/images/szolgaltatasok/olajcsere.jpg" alt="Avatar" className="image" />
-          <div className="middle9">
+          <div className="middle">
             <div className="text">Olajcsere</div>
           </div>  
         </div>
