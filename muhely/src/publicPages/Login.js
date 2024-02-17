1d403cd87433c18808393d03b94569cba888d0b0
szludora <szlucska.dora@gmail.com>
2024-02-17 12:04:03 +0100

    login szöveg

--- muhely/src/publicPages/Login.js
+++ muhely/src/publicPages/Login.js
@@ -12,9 +12,6 @@ export default function Login() {
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -25,7 +22,7 @@ export default function Login() {
             <Form.Check type="checkbox" label="Remember me" />
           </Form.Group>
           <Button variant="primary" type="submit">
-            Submit
+            Login
           </Button>
         </Form>
       </Col>
