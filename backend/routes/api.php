cc7d227ae8a2a7d8b7b8c73e4ab05ad4da57e0aa
Holylogus <hamar.patrik94@gmail.com>
2024-03-04 20:08:19 +0100

    AutoController + api végpontok + thunder tesztelés

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AutoController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -26,3 +27,11 @@ Route::get('/users{id}',[UserController::class, 'show']);
 Route::post('/users',[UserController::class, 'store']);
 Route::post('/users{id}',[UserController::class, 'update']);
 Route::delete('/users{id}',[UserController::class, 'destroy']);
+
+//Auto alap útvonalak
+Route::get('/auto',[AutoController::class, 'index']);
+Route::get('/auto{id}',[AutoController::class, 'show']);
+Route::post('/auto',[AutoController::class, 'store']);
+Route::post('/auto{id}',[AutoController::class, 'update']);
+Route::delete('/auto{id}',[AutoController::class, 'destroy']);
+
